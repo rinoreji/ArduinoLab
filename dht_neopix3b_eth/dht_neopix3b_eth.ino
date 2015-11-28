7076cc303d7ae5621cb88e6ed629d6db0653415d
rinoreji <mailrino@gmail.com>
2015-11-28 23:11:48 +0530

    Added LED (NeoPixel) to the project Adjusted some function for more memory

--- dht_neopix3b_eth/dht_neopix3b_eth.ino
+++ dht_neopix3b_eth/dht_neopix3b_eth.ino
@@ -31,10 +31,11 @@ DHT dht(DHTPIN, DHTTYPE);
 Adafruit_ST7735 tft = Adafruit_ST7735(cs, dc, mosi, sclk, rt); //rst ist schon belegt
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
-float humidity; float temperature;
+//float humidity; float temperature;
 unsigned long nextSensorRead;
 unsigned long nextPagePost;
 char hum[6]; char temp[6];
+int mxh = 0, mxt = 0, mnh = 0, mnt = 0;
 
 // initialize the library instance:
 EthernetClient client;
@@ -51,59 +52,24 @@ char pageName[] = "/temperature/web/device/upload/";
 String serverResponse;
 bool hasNewSensorValue = false;
 
+/*
 int freeRam () {
   extern int __heap_start, *__brkval;
   int v;
   return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval);
 }
+*/
 
 void setup() {
   Serial.begin(9600); Serial.println(F("Booting ..."));
 
-  initDispaly();
-  initEthernet();
-
-  nextSensorRead = millis() + 500;
-  nextPagePost = millis() + 500;
-}
-
-void loop() {
-  if (((signed long)(millis() - nextSensorRead)) > 0) {
-    Serial.println(freeRam());
-    humidity = dht.readHumidity();
-    temperature = dht.readTemperature();
-    hasNewSensorValue = true;
-
-    updateDisplay(humidity, temperature);
-    nextSensorRead = millis() + 10000;
-  }
-
-  if (((signed long)(millis() - nextPagePost)) > 0 && hasNewSensorValue) {
-    char postBuffer[80];
-    // postBuffer must be url encoded.
-    sprintf(postBuffer, "mac=%s&Temperature=%s&Humidity=%s", getMacAddress().c_str(), temp, hum);
-    Serial.println(postBuffer);
-    if (!postPage(server, port, pageName, postBuffer)) {
-      Serial.print(F("POST failed "));
-    }
-    else
-    {
-      hasNewSensorValue = false;
-    }
-    nextPagePost = millis() + nextPagePost;
-  }
-}
-
-void initDispaly() {
   Serial.println(F("Initializing display ..."));
   pixels.begin();
   tft.initR(INITR_BLACKTAB);   // initialize a ST7735S chip, black tab
   tft.setTextWrap(false); // Allow text to run off right edge
   tft.fillScreen(ST7735_BLACK);
   tft.setRotation(1);
-}
 
-void initEthernet() {
   // start the Ethernet connection:
   Serial.println(F("Starting ethernet ..."));
   if (Ethernet.begin(mac) == 0) {
@@ -115,6 +81,86 @@ void initEthernet() {
   // print the Ethernet board/shield's IP address:
   Serial.print(F("localIP address: "));
   Serial.println(Ethernet.localIP());
+
+  nextSensorRead = millis() + 500;
+  nextPagePost = millis() + 500;
+}
+
+void loop() {
+  if (((signed long)(millis() - nextSensorRead)) > 0) {
+    //Serial.println(freeRam());
+
+    hasNewSensorValue = true;
+
+    dtostrf(dht.readHumidity(), 4, 1, hum);  //4 is mininum width, 3 is precision;
+    dtostrf(dht.readTemperature(), 4, 1, temp);
+
+    char sensorValueBuffer[40];
+    sprintf(sensorValueBuffer, "Sensor values: %s - %s", hum, temp);
+    Serial.println(sensorValueBuffer);
+
+    //LED control
+    if (int(hum) >= mxh) {
+      showalarm (3, 100, 100, 0, 1, 1, 1, 20, 5);
+    }
+    if (int(hum) <= mnh) {
+      showalarm (3, 0, 200, 0, 1, 1, 1, 80, 5);
+    }
+    if (int(temp) >= mxh) {
+      showalarm (0, 100, 0, 0, 1, 1, 1, 20, 5);
+    }
+    if (int(temp) <= mxh) {
+      showalarm (0, 0, 0, 200, 1, 1, 1, 80, 5);
+    }
+
+    //Display
+    tft.fillRect(0,17,tft.width(),tft.height()-17,ST7735_BLACK);
+      tft.setRotation(1);
+      tft.setCursor(10, 18);
+      tft.setTextColor(ST7735_YELLOW);
+      tft.setTextSize(6);
+      tft.println(hum);
+      tft.setTextColor(ST7735_RED);
+      tft.setTextSize(6);
+      tft.setCursor(10, 69);
+      tft.println(temp);
+/*      
+    tft.fillRect(0, 14, tft.width(), tft.height() - 17, ST7735_BLACK);
+
+    tft.setCursor(10, 15);
+    tft.setTextColor(ST7735_YELLOW);
+    tft.setTextSize(3);
+    tft.println(hum);
+
+    tft.setCursor(10, 40);
+    tft.setTextColor(ST7735_RED);
+    tft.setTextSize(3);
+    tft.println(temp);
+
+    tft.setCursor(0, 68);
+    tft.setTextColor(ST7735_BLUE);
+    tft.setTextSize(1);
+    char diplayBuffer[80];
+    sprintf(diplayBuffer, " Server: %s\n\n Mac: %s\n\n LocalIP: %s", server, getMacAddress().c_str(), getIpAddress().c_str());
+    tft.println(diplayBuffer);
+*/
+    nextSensorRead = millis() + 5000;
+  }
+
+  if (((signed long)(millis() - nextPagePost)) > 0 && hasNewSensorValue) {
+    char postBuffer[80];
+    // postBuffer must be url encoded.
+    sprintf(postBuffer, "mac=%s&Temperature=%s&Humidity=%s", getMacAddress().c_str(), temp, hum);
+    Serial.println(postBuffer);
+    if (!postPage(postBuffer)) {
+      Serial.print(F("POST failed "));
+    }
+    else
+    {
+      hasNewSensorValue = false;
+    }
+    nextPagePost = millis() + nextPagePost;
+  }
 }
 
 //Mac address to string (For display purpose)
@@ -125,26 +171,26 @@ String getMacAddress() {
   }
   return macAddress;
 }
-
+/*
 //LocalIP address to string (For display purpose)
 String getIpAddress() {
   IPAddress address = Ethernet.localIP();
   return String(address[0]) + "." + String(address[1]) + "." + String(address[2]) + "." + String(address[3]);
 }
-
+*/
 //Connect to a server and do a POST
-byte postPage(char* domainBuffer, int thisPort, char* page, char* thisData)
+byte postPage(char* thisData)
 {
   Serial.println(F("connecting to remote server..."));
 
-  if (client.connect(domainBuffer, thisPort) == 1)
+  if (client.connect(server, port) == 1)
   {
     Serial.println(F("connected"));
     char outBuffer[80];
     // send the header
-    sprintf(outBuffer, "POST %s HTTP/1.1", page);
+    sprintf(outBuffer, "POST %s HTTP/1.1", pageName);
     client.println(outBuffer);
-    sprintf(outBuffer, "Host: %s", domainBuffer);
+    sprintf(outBuffer, "Host: %s", server);
     client.println(outBuffer);
     client.println(F("Connection: close\r\nContent-Type: application/x-www-form-urlencoded"));
     sprintf(outBuffer, "Content-Length: %u\r\n", strlen(thisData));
@@ -219,29 +265,17 @@ int getValue(String key, String data) {
   return 0;
 }
 
-//Updates display with current sensor values, Server address, Mac, LocalIP
-void updateDisplay(float humidity, float temperature) {
-  dtostrf(humidity, 4, 1, hum);  //4 is mininum width, 3 is precision;
-  dtostrf(temperature, 4, 1, temp);
-  char sensorValueBuffer[40];
-  sprintf(sensorValueBuffer, "Sensor values: %s - %s", hum, temp);
-  Serial.println(sensorValueBuffer);
-  tft.fillRect(0, 14, tft.width(), tft.height() - 17, ST7735_BLACK);
-
-  tft.setCursor(10, 15);
-  tft.setTextColor(ST7735_YELLOW);
-  tft.setTextSize(3);
-  tft.println(hum);
-
-  tft.setCursor(10, 40);
-  tft.setTextColor(ST7735_RED);
-  tft.setTextSize(3);
-  tft.println(temp);
-
-  tft.setCursor(0, 68);
-  tft.setTextColor(ST7735_BLUE);
-  tft.setTextSize(1);
-  char diplayBuffer[80];
-  sprintf(diplayBuffer, " Server: %s\n\n Mac: %s\n\n LocalIP: %s", server, getMacAddress().c_str(), getIpAddress().c_str());
-  tft.println(diplayBuffer);
+int showalarm(int up, int r, int g, int b, int r_off, int g_off, int b_off , int del, int times) {
+  //Serial.println();Serial.println("alarm: ");
+  // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+  for (int k = 0; k < times; k++) {
+    for (int i = 0; i < 3; i++) {
+      pixels.setPixelColor(i + up, pixels.Color(r, g, b));
+    }
+    pixels.show(); delay(del);
+    for (int i = 3; i >= 0; i--) {
+      pixels.setPixelColor(i + up, pixels.Color(r_off, g_off, b_off));
+    }
+    pixels.show(); delay(del);
+  }
 }
